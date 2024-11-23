493406d3bb806cb91ad63263b10d8aea41f83c95
Waffle <60797216+wafflethings@users.noreply.github.com>
2024-11-23 19:13:30 +0000

    discord link + gz optimization

--- EndlessDelivery/Components/Chimney.cs
+++ EndlessDelivery/Components/Chimney.cs
@@ -174,6 +174,11 @@ public class Chimney : MonoBehaviour
     // called by event in Assets/Delivery/Prefabs/Level/Chimney/Chimney.prefab > Pit/Teleport Zone/BoxCollider
     public void WarpPlayerToNextRoom()
     {
+        if (GameManager.Instance.PreviousRoom.TryGetComponent(out GoreZone goreZone))
+        {
+            goreZone.ResetGibs();
+        }
+
         Destroy(GameManager.Instance.PreviousRoom.gameObject);
         Vector3 tpOffset = NewMovement.Instance.transform.position - Teleporter.transform.position;
         NewMovement.Instance.transform.position = GameManager.Instance.CurrentRoom.SpawnPoint.transform.position + tpOffset.Only(Axis.X, Axis.Z);
