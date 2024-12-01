b4323f40604822cee751c11a10f5c850fb04fe87
Waffle <60797216+wafflethings@users.noreply.github.com>
2024-12-01 03:43:58 +0000

    achivements

---
+++ EndlessDelivery/Achievements/EventAchievementGiver.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using EndlessDelivery.Api.Requests;
+using EndlessDelivery.Online;
+using EndlessDelivery.UI;
+using UnityEngine;
+
+namespace EndlessDelivery.Achievements;
+
+public class EventAchievementGiver : MonoBehaviour
+{
+    public void Give(string id)
+    {
+        AchievementHud.Instance.AddAchievement(OnlineFunctionality.LastFetchedContent.Achievements[id]);
+        Task.Run(() => OnlineFunctionality.Context.GrantAchievement(id));
+    }
+}
