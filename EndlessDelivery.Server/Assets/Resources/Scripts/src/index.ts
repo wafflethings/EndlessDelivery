c37ddc1141cd29fa6c3b25b418a2d6484395982b
Waffle <60797216+wafflethings@users.noreply.github.com>
2024-12-09 23:29:56 +0000

    pointerup not down

--- EndlessDelivery.Server/Assets/Resources/Scripts/src/index.ts
+++ EndlessDelivery.Server/Assets/Resources/Scripts/src/index.ts
@@ -6,27 +6,27 @@ let elements : NodeListOf<Element> = document.querySelectorAll(".score-box")
 elements.forEach(function (element : Element) : void {
     let primary : HTMLDivElement | null = element.querySelector(".leaderboard-primary-content");;
     let secondary : HTMLDivElement | null = element.querySelector(".leaderboard-secondary-content");
-    
+
     if (primary == null || secondary == null) {
         return;
     }
-    
+
     secondary.setAttribute("start-height", secondary.offsetHeight.toString());
     secondary.setAttribute("enabled", "false");
     secondary.style.visibility = "hidden";
     secondary.style.maxHeight = "0";
 
-    primary.addEventListener("pointerdown", function () : void {
+    primary.addEventListener("pointerup", function () : void {
         if (secondary == null) {
             return;
         }
-        
+
         let goingVisible : boolean  = secondary.getAttribute("enabled") == "false";
         secondary.setAttribute("enabled", goingVisible ? "true" : "false");
 
         let targetHeight : string = goingVisible ? secondary.getAttribute("start-height") ?? "0" : "0";
         secondary.animate({ maxHeight: targetHeight + "px" }, { duration: 250, fill: "forwards", easing: "ease-out" });
-        
+
         if (goingVisible) {
             secondary.style.visibility = "visible";
         } else {
