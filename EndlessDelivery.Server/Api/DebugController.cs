7b68b2a7f5928ecc78f5ec19a44548bf066fe5af
Waffle <60797216+wafflethings@users.noreply.github.com>
2024-12-14 04:23:03 +0000

    add item grant giving

--- EndlessDelivery.Server/Api/DebugController.cs
+++ EndlessDelivery.Server/Api/DebugController.cs
@@ -1,6 +1,11 @@
-﻿using EndlessDelivery.Server.Api.Steam;
+﻿using EndlessDelivery.Common.ContentFile;
+using EndlessDelivery.Server.Api.ContentFile;
+using EndlessDelivery.Server.Api.Steam;
+using EndlessDelivery.Server.Api.Users;
+using EndlessDelivery.Server.Database;
 using EndlessDelivery.Server.Resources;
 using EndlessDelivery.Server.Website;
+using EndlessDelivery.UI;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EndlessDelivery.Server.Api;
@@ -22,6 +27,44 @@ public class DebugController : Controller
             resourcePair.Reset();
         }
 
+        return StatusCode(StatusCodes.Status200OK, ":3");
+    }
+
+    [HttpGet("check_grants")]
+    public async Task<ObjectResult> CheckGrants()
+    {
+        if (!HttpContext.TryGetLoggedInPlayer(out SteamUser steamUser) || !(await steamUser.GetUserModel()).Admin)
+        {
+            return StatusCode(StatusCodes.Status403Forbidden, "Go away!!!!");
+        }
+
+        Cms cms = ContentController.CurrentContent;
+        await using DeliveryDbContext dbContext = new();
+
+        foreach (UserModel user in dbContext.Users)
+        {
+            foreach (string claimedDay in user.ClaimedDays)
+            {
+                CalendarReward day = cms.CalendarRewards[claimedDay];
+
+                if (!day.HasItem)
+                {
+                    continue;
+                }
+
+                string dayRewardId = cms.CalendarRewards[claimedDay].ItemId;
+
+                if (user.OwnedItemIds.Contains(dayRewardId))
+                {
+                    continue;
+                }
+
+                user.OwnedItemIds.Add(dayRewardId);
+                dbContext.Users.Update(user);
+            }
+        }
+
+        await dbContext.SaveChangesAsync();
         return StatusCode(StatusCodes.Status200OK, ":3");
     }
 }
